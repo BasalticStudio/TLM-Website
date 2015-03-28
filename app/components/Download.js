c1212d401e5ef832f55b5b17de13a5e601051345
蒼時弦也 <elct9620@frost.tw>
2015-03-28 17:01:30 +0800

    Imporve download section display

--- app/components/Download.js
+++ app/components/Download.js
@@ -8,8 +8,14 @@ export default class Download extends React.Component {
     render() {
         return (
             <section id="download" className="container">
-                <img src="/images/icons/download.svg" width="64"/>
-                <p>試玩版即將開放</p>
+                <h1 className="section-title small">下載遊戲</h1>
+                <div className="landing-hero-image">
+                    <img src="/images/Screenshots/download.jpg" />
+                    <div className="landing-hero-entry text-center">
+                        <img src="/images/icons/download.svg" width="64"/>
+                        <p>試玩版即將開放</p>
+                    </div>
+                </div>
             </section>
         )
     }
