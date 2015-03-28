e108dfe79bb09c2783d1804f48ebe2aa96fd1cac
蒼時弦也 <elct9620@frost.tw>
2015-03-28 17:22:48 +0800

    Add system sectio with notice message

--- app/components/System.js
+++ app/components/System.js
@@ -7,8 +7,11 @@ import React from 'react'
 export default class System extends React.Component {
     render() {
         return(
-            <section id="system" className="section">
-
+            <section id="system" className="container section">
+                <h1 className="section-title small">遊戲系統</h1>
+                <div className="alert text-center">
+                    詳細介紹將陸續登場
+                </div>
             </section>
         )
     }
