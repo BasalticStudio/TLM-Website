c7bb2d40913035ed79513aecf3536d97bd76533b
蒼時弦也 <elct9620@frost.tw>
2016-02-03 16:18:22 +0800

    Update download message

--- app/components/Download.js
+++ app/components/Download.js
@@ -12,17 +12,14 @@ export default class Download extends React.Component {
                 <div className="landing-hero-image">
                     <img src="/images/Screenshots/download.jpg" />
                     <div className="landing-hero-entry">
-                        <h3>試玩版下載分流</h3>
+                        <h3>遊戲已在 Steam 商店發布</h3>
                         <p>
-                            <a className="link-icon" href="https://mega.co.nz/#!T4JRED7B!lxnjKW13eOdEuPEaDGMMBrTiK-g19My39PTezy8B4ag" target="_blank">
-                                <img src="/images/ICONs/mega.svg" width="64" />
-                            </a>
-                            <a className="link-icon" href="http://www.mediafire.com/download/1u9ve2fuz6fkadv/The_Lost_Mythologies_-_Preview_150403.zip" target="_blank">
-                                <img src="/images/ICONs/mediafile.svg" width="64"/>
+                            <a className="link-icon" href="http://store.steampowered.com/app/372540" target="_blank">
+                                <img src="/images/ICONs/steam.svg" width="64" />
                             </a>
                         </p>
                         <p>
-                            目前試玩版仍存在許多問題，如果有任何問題可以在我們的粉絲專頁上留言建議。
+                            遊戲將會持續更新，若有任何建議歡迎透過 Steam 討論區提出建議。
                         </p>
                     </div>
                 </div>
